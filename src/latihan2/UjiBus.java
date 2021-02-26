d84e93bb0c6c84e4158b422854af73c221524603
MochRiswan <moch_riswan_29rpl@studen.smktelkom-mlg.sch.id>
2021-02-26 16:43:31 +0700

    Latihan 2

---
+++ src/latihan2/UjiBus.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class UjiBus {
+    
+    public static void main(String[] args) {
+        
+        //membuat objek busBesar dari class Bus
+        Bus busBesar = new Bus(40);
+        busBesar.cetak();
+        
+        //Penambahan penumpang
+        busBesar.addPenumpang(15);//Menambahkan 15 penumpang
+        busBesar.cetak();
+        
+        //Penambahan penumpang
+        busBesar.addPenumpang(5);//Menambahkan 5 penumpang
+        busBesar.cetak();
+        
+        //Penambahan penumpang
+        busBesar.addPenumpang(26);//Menambahkan 26 penumpang
+        busBesar.cetak();
+        
+    }
+    
+}
